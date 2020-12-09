4e08af27dabc2c9aba4288b86adca586a4e6b929
Owen Webb <owebb@umich.edu>
2020-12-09 18:29:04 +0000

    Need to compile final bc with clang

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ fname=$1
 arg=$2
 
 # compile CPI lib to LLVM bitcode, compile prog to LLVM bitcode
-clang -emit-llvm cpi_pass/cpi.cpp -c -o cpi.bc &&
-clang -emit-llvm demos/$fname.cpp -c -o $fname.bc &&
+clang -std=c++11 -stdlib=libc++ -emit-llvm cpi_pass/cpi.cpp -c -o cpi.bc &&
+clang -std=c++11 -stdlib=libc++ -emit-llvm demos/$fname.cpp -c -o $fname.bc &&
 
 # run CPI pass on prog (inserts calls to CPI lib)
 opt -load build/cpi_pass/CPI.so -cpi < $fname.bc > $fname.opt.bc 2> /dev/null &&
@@ -17,8 +17,10 @@ opt -load build/cpi_pass/CPI.so -cpi < $fname.bc > $fname.opt.bc 2> /dev/null &&
 # link CPI lib to instrumented prog
 llvm-link cpi.bc $fname.opt.bc -o prog.bc &&
 
-# run prog
-lli prog.bc $arg &&
+# compile and run prog
+clang -lstdc++ prog.bc $arg -o fname &&
+chmod +x fname &&
+./fname
 
 # cleanup bitcode files
 rm cpi.bc &&
