3325d66e69de700bdac089b93f4a69fb73fd88c2
Johnathan Yang <johnnyhy@eecs583a.eecs.umich.edu>
2020-12-10 05:11:14 +0000

    new build with insecure compile

--- build.sh
+++ build.sh
@@ -9,19 +9,20 @@ arg=$2
 
 # compile CPI lib and user prog to LLVM bitcode
 clang++ -emit-llvm src/cpi.cpp -c -o cpi.bc &&
-clang++ -g3 -emit-llvm demos/$fname.cpp -c -o $fname.bc &&
+clang++ -g3 -fno-stack-protector -emit-llvm demos/$fname.cpp -c -o $fname.bc &&
 
 # run CPI pass on prog (instruments calls to CPI lib)
 opt -load build/cpi_pass/CPI.so -cpi < $fname.bc > $fname.opt.bc &&
 
 # link CPI lib to instrumented prog
-llvm-link cpi.bc $fname.opt.bc -o prog.bc &&
+llvm-link cpi.bc $fname.opt.bc -o $fname.cpilib.bc &&
 
 # compile and run prog
-clang++ -fno-stack-protector -g3 -lssl -lcrypto prog.bc -o $fname.demo &&
+clang++ -fno-stack-protector -g3 -lssl -lcrypto $fname.cpilib.bc -o $fname.cpilib &&
+clang++ -fno-stack-protector -g3 demos/$fname.cpp -o $fname.insecure
 
 # cleanup bitcode files
 rm cpi.bc &&
 rm $fname.bc &&
 rm $fname.opt.bc &&
-rm prog.bc
+rm $fname.cpilib.bc
