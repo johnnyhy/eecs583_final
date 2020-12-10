80f204d89393530eb402dfc11c0852b012097f02
Owen Webb <owebb@umich.edu>
2020-12-10 02:51:37 +0000

    change run.sh to build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -x
 
 # get name of prog to run
 fname=$1
-arg=$2
+arg=$2   
 
 # compile CPI lib and user prog to LLVM bitcode
 clang++ -emit-llvm src/cpi.cpp -c -o cpi.bc &&
@@ -18,9 +18,7 @@ opt -load build/cpi_pass/CPI.so -cpi < $fname.bc > $fname.opt.bc &&
 llvm-link cpi.bc $fname.opt.bc -o prog.bc &&
 
 # compile and run prog
-clang++ -lssl -lcrypto prog.bc -o $fname.demo &&
-chmod +x $fname.demo &&
-./$fname.demo $arg &&
+clang++ -fno-address-sanitizer -fno-memsafety -lssl -lcrypto prog.bc -o $fname.demo &&
 
 # cleanup bitcode files
 rm cpi.bc &&
