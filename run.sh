727238829e85db4533f85a2db64cd5ce76170d70
Owen Webb <owebb@umich.edu>
2020-12-10 01:40:55 +0000

    Decouple CPI lib

--- run.sh
+++ run.sh
@@ -7,24 +7,23 @@ set -x
 fname=$1
 arg=$2
 
-# compile CPI lib to LLVM bitcode, compile prog to LLVM bitcode
-clang++ -emit-llvm cpi_pass/cpi.cpp -c -o cpi.bc &&
+# compile CPI lib and user prog to LLVM bitcode
+clang++ -emit-llvm src/cpi.cpp -c -o cpi.bc &&
 clang++ -emit-llvm demos/$fname.cpp -c -o $fname.bc &&
 
-# run CPI pass on prog (inserts calls to CPI lib)
+# run CPI pass on prog (instruments calls to CPI lib)
 opt -load build/cpi_pass/CPI.so -cpi < $fname.bc > $fname.opt.bc &&
 
 # link CPI lib to instrumented prog
 llvm-link cpi.bc $fname.opt.bc -o prog.bc &&
 
 # compile and run prog
-clang++ -lssl -lcrypto prog.bc -o $fname &&
+clang++ -lssl -lcrypto prog.bc -o $fname.demo &&
 chmod +x $fname &&
-./$fname $arg
+./$fname $arg &&
 
 # cleanup bitcode files
 rm cpi.bc &&
-rm $fname
 rm $fname.bc &&
 rm $fname.opt.bc &&
 rm prog.bc
